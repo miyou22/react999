29f62d3492bfae82e16d8eb7951077ad6b4cca27
miyou22 <87642297+miyou22@users.noreply.github.com>
2021-11-11 14:49:19 +0900

    2021/2/11_1

---
+++ src/components/pages/Reference.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import React from 'react';
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
+import Contents from '../layouts/Contents';
+import Loading from '../layouts/Loading';
+import WrapTitle from '../layouts/WrapTitle';
+import ReferInfo from '../info/ReferInfo';
+import ContInfo from '../layouts/ContInfo';
+
+class Reference extends React.Component {
+    state = {
+        isLoading: true,
+        refers: []
+    }
+    getRefer = async () => {
+        const {
+            data: {
+                data : {htmlRefer},
+            },
+        } = await axios.get("https://webstoryboy.github.io/react5001/src/assets/json/refer.json");
+        this.setState({ refers : htmlRefer, isLoading: false})
+
+    //htmlRefer
+
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.getRefer();
+        }, 2000)
+    }
+
+    render(){
+        const {isLoading, refers} = this.state;
+
+        return (
+            <div>
+                {isLoading ? (
+                    <Loading />
+                ) : (
+                    <div>
+                        <Header />
+                        <Contents>
+                            <section id ="referCont">
+                                <div className = "container">
+                                    <WrapTitle text={['Reference', 'book']} />
+                                    <div className="refer__cont">
+                                        <div className="table">
+                                            <h3>HTML</h3>
+                                            <ul>
+                                                {refers.map((refer) => (
+                                                    <ReferInfo 
+                                                        key={refer.id}
+                                                        link={refer.link}
+                                                        id={refer.id}
+                                                        title={refer.title}
+                                                        desc1={refer.desc1}
+                                                        desc2={refer.desc2}
+                                                        element={refer.element}
+                                                        tag={refer.tag}
+                                                        version={refer.version}
+                                                        view={refer.view}
+                                                        use={refer.use}
+                                                        definition={refer.definition}
+                                                    />
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <ContInfo />
+                        </Contents>
+                        <Footer />
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+export default Reference;
